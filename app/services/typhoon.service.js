2938095cc2104fdbc38423a32d2eec4eceea8157
djing <302624318@qq.com>
2018-03-15 22:05:43 +0800

    change app name from myApp to typhoon

--- app/services/typhoon.service.js
+++ app/services/typhoon.service.js
@@ -1,6 +1,31 @@
 angular.module('typhoon.services')
-.service('Typhoon', function () {
+.service('Typhoon', function ($http) {
 	var pointsArray = [];
+	
+	function level(strong) {
+		switch (strong) {
+			case "超强台风(Super TY)":
+				return 0;
+				break;
+			case "强台风(STY)":
+				return 1;
+				break;
+			case "台风(TY)":
+				return 2;
+				break;
+			case "强热带风暴(STS)":
+				return 3;
+				break;
+			case "热带风暴(TS)":
+				return 4;
+				break;
+			case "热带低压(TD)":
+				return 5;
+				break;
+			default:
+				return -1;
+		}
+	}
 	
 	var promise = $http.get('lib/Meranti.json').success(function (data) {
 		var dataPoints = data[0].points;
